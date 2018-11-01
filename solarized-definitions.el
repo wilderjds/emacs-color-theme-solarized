652425765952d54d6117ed264ca637a4edb553d3
Jacopo De Simoi <jacopods@gmail.com>
2018-11-01 11:11:24 +0100

    Remove invert video from modeline

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -252,9 +252,9 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
                 (menu (,@fg-base0 ,@bg-base02))
                 (minibuffer-prompt (,@fmt-bold ,@fg-cyan)) ; Question
                 (mode-line ; StatusLine
-                 (,@fg-base1 ,@bg-base02 ,@fmt-revbb :box nil))
+                 (,@fg-base01 ,@bg-base3 :box nil))
                 (mode-line-inactive    ; StatusLineNC
-                 (,@fg-base00 ,@bg-base02 ,@fmt-revbb :box nil))
+                 (,@fg-base01 ,@bg-base2 :box nil))
                 (region (,@fg-base01 ,@bg-base03 ,@fmt-revbb)) ; Visual
                 (secondary-selection (,@bg-base02))
                 (shadow (,@fg-base01))
